2299f7ec7822120f2a03422b6309dfdc4b0fdb1f
HBcao233 <hbcaoqaq@gmail.com>
2024-10-28 15:20:35 +0800

    feat: 同步bot更名

--- main.go
+++ main.go
@@ -4,14 +4,14 @@ import (
 	"os"
 	"os/signal"
 
-	"github.com/Logiase/MiraiGo-Template/bot"
+	"github.com/Logiase/MiraiGo-Template/client"
 
 	_ "github.com/Logiase/MiraiGo-Template/plugins/ping"
 )
 
 func main() {
-	bot.Init()
-	bot.Login()
+	client.Init()
+	client.Login()
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt)
 	<-ch
